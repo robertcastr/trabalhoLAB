3c4772c54fa94807c67dc1d1d33f37ec61e5bd92
robertcastr <robertwenitt@gmail.com>
2022-06-12 01:02:32 -0300

    Add files via upload

---
+++ empresa.sql
@@ -0,0 +1,14 @@
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoa`
+--
+
+CREATE TABLE `pessoa` (
+  `id` int(11) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  `login` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `endereco` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `numero` varchar(20) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
